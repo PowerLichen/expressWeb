8bc4585511c825e225f52405814532ff07274cc0
PowerLichen <dltm135@gmail.com>
2021-07-12 17:25:02 +0900

    Add modulize page view

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser')
 var compression = require('compression')
 var sanitizeHtml = require('sanitize-html')
 var template = require('./lib/template.js')
+var topicRouter = require('./routes/topic') 
 
 //서버 구동 시, middleware가 자동실행
 app.use(express.static('public'));
@@ -32,109 +33,12 @@ app.get('/', function (req, res) {
     <h2>${title}</h2><p>${description}</p>
     <img src="/images/hello.jpg" style = "width:300px; display:block; margin-top:10px;">
     `,
-    '<a href="/create">create</a>'
+    '<a href="/topic/create">create</a>'
   );
   res.send(html);
 });
 
-app.get('/page/:pageId/', function (req, res, next) {
-  var filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      var title = req.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(req.list)
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action='/delete_process' method='post'>
-            <input type='hidden' name='id' value='${sanitizedTitle}'>
-            <input type='submit' value='delete'>
-          </form>
-          `
-      );
-      res.send(html);
-    }
-  });
-});
-
-app.get('/create', function (req, res) {
-  var title = 'WEB - create'
-  var list = template.list(req.list)
-  var html = template.HTML(title, list, `
-    <form action="/create_process" method="POST">
-        <p><input type="text" name='title' placeholder='title'></p>
-        <p>
-            <textarea name='description' placeholder='description'></textarea>
-        </p>
-        <p>
-            <input type="submit">
-        </p>
-    </form>
-    `, '');
-  res.send(html);
-});
-
-app.post('/create_process', function (req, res) {
-  var post = req.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    res.redirect(`/page/${title}`);
-  });
-});
-
-app.get('/update/:pageId', function (req, res) {
-  var filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = req.params.pageId;
-    var list = template.list(req.list)
-    var html = template.HTML(title, list,
-      `
-        <form action="/update_process" method="POST">
-            <input type='hidden' name='id' value='${title}'>
-            <p><input type="text" name='title' placeholder='title', value='${title}'></p>
-            <p>
-                <textarea name='description' placeholder='description'>${description}</textarea>
-            </p>
-            <p>
-                <input type="submit">
-            </p>
-        </form>
-        `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-    );
-    res.send(html);
-  });
-});
-
-app.post('/update_process', function (req, res) {
-  var post = req.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${filteredId}`, `data/${title}`, function (err) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      res.redirect(`/page/${title}`);
-    });
-  });
-});
-
-app.post('/delete_process', function (req, res) {
-  var post = req.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (err) {
-    res.redirect('/');
-  });
-});
+app.use('/topic', topicRouter);
 
 app.use(function (req, res, next) {
   res.status(404).send('Sorry cannot find that!');
